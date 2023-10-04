f9d897f76b1c882ef830b6e28ab0c2d839726275
Shahadat Siddikee Shawon <129535921+SHAWON0c@users.noreply.github.com>
2023-10-04 20:21:18 +0600

    Add files via upload

--- Practice day -4/R_word_game.cpp
+++ Practice day -4/R_word_game.cpp
@@ -1,1 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nn cout << "NO" << endl
+#define yy cout << "YES" << endl
 
+int main() {
+
+
+          int t;
+           cin>>t;
+           while(t--){
+        int n;
+         cin>>n;
+
+        map<string,int> freq;
+
+        vector<string> a[3];
+        for(int i=0;i<3;i++){
+            a[i].resize(n);
+            for(int j=0;j<n;j++){
+                cin>>a[i][j];
+                freq[a[i][j]]++;
+            }
+        }
+        int p[3]={0};
+        for(int i=0;i<3;i++){
+            for(int j=0;j<n;j++){
+                if(freq[a[i][j]]==1){
+                    p[i]+=3;
+                }
+                else if(freq[a[i][j]]==2){
+                    p[i]+=1;
+                }
+            }
+        }
+
+        cout<<p[0]<<" "<<p[1]<<" "<<p[2]<<endl;
+
+    }
+
+
+
+
+    return 0;
+}
